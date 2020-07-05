7bbb5dc1dfca7664d56a1696c6677769e4e2b093
Michał Flajszer <flajszermichal@hotmail.com>
2020-07-05 12:19:30 +0200

    first part of work

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,95 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
+      console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion(){
+
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+
+      const clickableTrigger = thisProduct.Accordion;
+
+      /* START: click event listener to trigger */
+
+      clickableTrigger.addEventListener('click', function(){
+
+        /* prevent default action for event */
+
+        event.preventDefault();
+
+        /* toggle active class on element of thisProduct */
+
+        thisProduct.element.classList.toggle('product__header');
+
+        /* find all active products */
+
+        const activeProducts = document.querySelectorAll('product', 'active');
+
+        /* START LOOP: for each active product */
+
+        for (let activeProduct of activeProducts){
+
+          /* START: if the active product isn't the element of thisProduct */
+
+          if (activeProduct != thisProduct.element){
+
+            /* remove class active for the active product */
+
+            activeProduct.classList.remove('active');
+
+            /* END: if the active product isn't the element of thisProduct */
+
+          }
+
+          /* END LOOP: for each active product */
+
+        }
+
+        /* END: click event listener to trigger */
+
+      });
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +148,14 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
+
+
   app.init();
+
 }
